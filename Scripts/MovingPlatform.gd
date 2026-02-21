be4491d1d8d44197b5fd9b62876be3216d990714
Sandwall <17184361+Sandwall@users.noreply.github.com>
2026-02-21 03:19:39 -0500

    added a template of a moving platform

--- Scripts/MovingPlatform.gd
+++ Scripts/MovingPlatform.gd
@@ -1,12 +1,6 @@
-extends Node3D
+extends PathFollow3D
 
+@export var timeLength := 5.0
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	progress += delta / timeLength
