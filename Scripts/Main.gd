c5b18b48eb04b5d70e2a8c266cfee92fc13ec675
Sandwall <17184361+Sandwall@users.noreply.github.com>
2026-02-22 02:54:06 -0500

    imported emails

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -17,3 +17,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("plr_pause"):
 		get_tree().paused = !get_tree().paused
 		pauseScreen.visible = get_tree().paused
+		if get_tree().paused:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
