7858f931c9f7203df49dca01f83045aabcb492f6
Om Wani <omwani0701@gmail.com>
2026-02-22 03:24:43 -0500

    Final Audio Changes pre submission

--- Scripts/Firewall.gd
+++ Scripts/Firewall.gd
@@ -1,6 +1,16 @@
 extends Node3D
 
+@onready var prevAllow = GlobalEmailManager.is_carrying_email
+
 func _process(delta):
 	var allow := !GlobalEmailManager.is_carrying_email
 	visible = allow
 	$StaticBody3D/CollisionShape3D.disabled = !allow
+
+	if allow != prevAllow:
+		if visible:
+			Wwise.post_event("PLAY_SFX_Firewall", $FireWall)
+		else:
+			Wwise.post_event("STOP_SFX_Firewall", $FireWall)
+
+	prevAllow = allow
